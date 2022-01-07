35988a0573354cd49a7cefad4c28559c6d77c0c6
djangorobert <djangocharm2020@gmail.com>
2022-01-06 21:02:56 -0600

    make it better

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import icons8 from './icons8-taco-48.png';
+import hamburger from './hamburger.png';
+class App extends Component {
+  state = {
+    reviews: []
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  async componentDidMount() {
+    try {
+      const res = await fetch('http://localhost:8000/reviews');
+      const reviews = await res.json();
+      this.setState({
+        reviews
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  render() {
+    return (
+
+
+      <div class="container">
+        <h2 class="title">Restraunt Reviews by: ReviewMe App <img src={icons8} alt="logo" /> <img src={hamburger} /></h2>
+
+        <table class="table table-dark table-striped">
+
+          <thead>
+            <tr>
+              <th>Name:</th>
+              <th>Reviews:</th>
+
+
+            </tr>
+
+          </thead>
+          {this.state.reviews.map(review => (
+            <tbody>
+              <tr>
+                <td><u>{review.restraunt_name}</u></td>
+                <td>"{review.comment}"</td>
+
+
+
+
+              </tr>
+            </tbody>
+          ))}
+        </table>
+      </div>
+
+
+    );
+  }
 }
 
 export default App;
